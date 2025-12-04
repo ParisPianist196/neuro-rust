694724878d19293e726eebe52fccd7d643ae9866
abigail <abigailhendrick96@gmail.com>
2025-12-04 12:17:42 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Resources
+[Building Spiking Neural Networks (SNNs) from Scratch](https://soney.github.io/snn-from-scratch/chapters/01%20-%20Root.html)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn potential(v_t: f64) -> f64 {
+    let tau = 0.04;
+    let energy_t = 1.0;
+    (1.0/tau * energy_t) + (1.0/tau * v_t)
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
